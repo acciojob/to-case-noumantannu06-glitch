d9ed64f87f4bacb0d50beec191fcddc63bfb267b
Md Nouman <noumantannu06@gmail.com>
2026-03-25 23:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function toCase(text) {
   // write your code here
+	function toCase(text) {
+  if (text.length === 0) return '-';
+  
+  const lower = text.toLowerCase();
+  const upper = text.toLowerCase().toUpperCase();
+  
+  return lower + '-' + upper;
+}
+
 }
 
 // DO not change the code below
