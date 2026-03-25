f072c3169ebd3261ec9da0276589dcbb0fa9e4e7
Md Nouman <noumantannu06@gmail.com>
2026-03-25 23:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function toCase(input) {
-    
+    // Step 1: Handle empty string
     if (input.trim() === "") {
         return ""; 
     }
 
-    
+    // Step 2: Split the string into words
     const words = input.trim().split(/\s+/); 
 
     // Step 3: Convert each word to lowercase and uppercase
@@ -19,9 +19,6 @@ function toCase(input) {
 // DO not change the code below
 const text = prompt("Enter text:");
 alert(toCase(text));
-
-
-}
 
 // DO not change the code below
 
